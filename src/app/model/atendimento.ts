026700e07e04f8a9010b8b43b97d72375c6bc1b9
Leonardo Viveiros <leonardoviveirossantos@gmail.com>
2018-09-24 01:03:02 -0300

    models

---
+++ src/app/model/atendimento.ts
@@ -0,0 +1,8 @@
+export interface IAtendimento {
+    id: string;
+    passageiro: string;
+    origem: string;
+    destino: string;
+    horario: Date;
+    status: string;
+}
---
+++ src/app/model/index.ts
@@ -0,0 +1,3 @@
+export * from './user';
+export * from './alert.message';
+export * from './atendimento';
